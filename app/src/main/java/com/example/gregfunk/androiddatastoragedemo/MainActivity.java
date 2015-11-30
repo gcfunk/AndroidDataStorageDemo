599411f37d4a52dbacc0c190e41bf06054c36012
Greg Funk <greg.funk@walltowall.com>
2015-11-30 15:28:30 -0500

    Get and set shared preferences

--- app/src/main/java/com/example/gregfunk/androiddatastoragedemo/MainActivity.java
+++ app/src/main/java/com/example/gregfunk/androiddatastoragedemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.gregfunk.androiddatastoragedemo;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //set and get a shared preference
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.gregfunk.androiddatastoragedemo", Context.MODE_PRIVATE);
+        sharedPreferences.edit().putString("username", "greg").apply();
+        String username = sharedPreferences.getString("username", "");
+        Log.i("username", username);
     }
 }
