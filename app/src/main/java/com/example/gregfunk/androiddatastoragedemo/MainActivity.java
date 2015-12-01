bde76f6c52fe55699aca0e72cdd19608d9b592b5
Greg Funk <greg.funk@walltowall.com>
2015-12-01 09:43:12 -0500

    Use support v7 toolbar

--- app/src/main/java/com/example/gregfunk/androiddatastoragedemo/MainActivity.java
+++ app/src/main/java/com/example/gregfunk/androiddatastoragedemo/MainActivity.java
@@ -4,10 +4,10 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-//import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         myToolbar.inflateMenu(R.menu.main_menu);
-        //setSupportActionBar(myToolbar);
+        setSupportActionBar(myToolbar);
 
 
         //set and get a shared preference
@@ -26,6 +26,12 @@ public class MainActivity extends AppCompatActivity {
         sharedPreferences.edit().putString("username", "greg").apply();
         String username = sharedPreferences.getString("username", "");
         Log.i("username", username);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 
     @Override
